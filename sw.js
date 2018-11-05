8800ec087eed37519b607d1533fc2e440bdeac09
Michael Karr <mdkarr01@gmail.com>
2018-11-05 14:26:55 -0600

    initial pwa setup

---
+++ sw.js
@@ -0,0 +1,16 @@
+var CACHE_STATIC_NAME = 'static-v1';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/js/app.js', ,
+  '/js/promise.js',
+  '/js/fetch.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/img/apple.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
